0594662cf557c43755cb784711fc395f61ed52c4
Luna Heyman <luna.heyman@proton.me>
2025-05-17 19:32:38 +0200

    feat(modules/nixos): add transparency to terminal

--- modules/nixos/home/gui/foot.nix
+++ modules/nixos/home/gui/foot.nix
@@ -3,13 +3,12 @@
     enable = true;
 
     settings = {
+      scrollback.indicator-position = "none";
+      colors.alpha = 0.85;
+
       main = {
         font = "monospace:size=12";
         dpi-aware = "yes";
-      };
-
-      scrollback = {
-        indicator-position = "none";
       };
     };
   };
