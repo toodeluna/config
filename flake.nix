b29ef38829cfd18f446caa01b04dfe12c782022a
Luna Heyman <luna.heyman@proton.me>
2025-10-11 16:28:19 +0200

    style: add treefmt and format code

--- flake.nix
+++ flake.nix
@@ -5,9 +5,26 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/default";
+
+    treefmt = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = import inputs.systems;
-  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.treefmt.flakeModule ];
+
+      perSystem.treefmt = {
+        projectRootFile = "flake.nix";
+
+        programs = {
+          nixfmt.enable = true;
+          prettier.enable = true;
+        };
+      };
+    };
 }
