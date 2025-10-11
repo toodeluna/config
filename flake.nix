6e768297fd5037dab59205941ab147fd477e0890
Luna Heyman <luna.heyman@proton.me>
2025-10-11 16:31:42 +0200

    refactor: improve directory structure

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   description = "My system configurations in a Nix flake";
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/default";
 
@@ -11,20 +13,4 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.treefmt.flakeModule ];
-
-      perSystem.treefmt = {
-        projectRootFile = "flake.nix";
-
-        programs = {
-          nixfmt.enable = true;
-          prettier.enable = true;
-        };
-      };
-    };
 }
---
+++ modules/flake/imports.nix
@@ -0,0 +1,6 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.treefmt.flakeModule
+  ];
+}
