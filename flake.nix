a2d45df307b73798a9f3f712d3026ffcbb4f43c5
Luna Heyman <luna@toodeluna.net>
2026-03-06 13:08:22 +0100

    refactor: add flake-parts

--- flake.nix
+++ flake.nix
@@ -1,12 +1,19 @@
 {
   description = "My system configurations in a Nix flake";
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ./flake;
 
   inputs = {
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
-  };
 
-  outputs = { self, nixpkgs }: {
+    systems = {
+      url = "github:nix-systems/default";
+    };
   };
 }
