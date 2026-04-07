abd013b9dcf4004808186e329607c3a7b15bdb94
Luna Heyman <luna@toodeluna.net>
2026-04-07 13:50:43 +0000

    feat(hosts/crona): add github to known ssh hosts

--- nix/hosts/crona/configuration.nix
+++ nix/hosts/crona/configuration.nix
@@ -97,6 +97,13 @@
     luna = ./home.nix;
   };
 
+  programs.ssh.knownHosts = {
+    github = {
+      hostNames = [ "github.com" ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    };
+  };
+
   programs.mango = {
     enable = true;
     package = inputs'.mangowm.packages.mango.override { enableXWayland = false; };
