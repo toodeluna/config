6f77320e5321b8963792644698ab5069fa3390d9
Luna Heyman <luna@toodeluna.net>
2026-04-07 09:17:23 +0000

    feat(hosts/crona): add xdg-user-dirs configuration

--- nix/hosts/crona/home.nix
+++ nix/hosts/crona/home.nix
@@ -26,6 +26,25 @@ in
     sources.palette = osConfig.catppuccin.sources.palette;
   };
 
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    desktop = "${homeDirectory}/desktop";
+    documents = "${homeDirectory}/documents";
+    download = "${homeDirectory}/downloads";
+    music = "${homeDirectory}/media/music";
+    pictures = "${homeDirectory}/media/photos";
+    publicShare = "${homeDirectory}/share";
+    templates = "${homeDirectory}/templates";
+    videos = "${homeDirectory}/media/videos";
+
+    extraConfig = {
+      CODE_DIR = "${homeDirectory}/code";
+      GAMES_DIR = "${homeDirectory}/media/games";
+    };
+  };
+
   programs.git = {
     enable = true;
 
