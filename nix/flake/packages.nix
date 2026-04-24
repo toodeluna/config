2d2a49eb0c26265206c1bccb3e88cc1bb7a9639c
Luna Heyman <luna@toodeluna.net>
2026-04-24 17:04:27 +0200

    fix: only export packages for their target platforms

--- nix/flake/packages.nix
+++ nix/flake/packages.nix
@@ -1,11 +1,16 @@
 { self, lib, ... }:
 {
   perSystem =
-    { pkgs, ... }:
-    {
+    { system, pkgs, ... }:
+    let
       packages = lib.packagesFromDirectoryRecursive {
         inherit (pkgs) callPackage;
         directory = "${self}/nix/packages";
       };
+    in
+    {
+      packages = lib.filterAttrs (
+        name: package: builtins.elem system (package.meta.platforms or [ ])
+      ) packages;
     };
 }
