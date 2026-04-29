753b9a5c610b7bcc37ae84d682ed5a5290fd72b7
Luna Heyman <luna@toodeluna.net>
2026-04-29 17:26:43 +0200

    refactor(flake): use treefmt module for qmlformat

--- nix/flake/treefmt.nix
+++ nix/flake/treefmt.nix
@@ -1,26 +1,17 @@
-{ inputs, lib, ... }:
+{ inputs, ... }:
 {
   imports = [ inputs.treefmt.flakeModule ];
 
-  perSystem =
-    { pkgs, ... }:
-    {
-      treefmt = {
-        projectRootFile = "flake.nix";
+  perSystem.treefmt = {
+    projectRootFile = "flake.nix";
 
-        programs = {
-          just.enable = true;
-          nixfmt.enable = true;
-          prettier.enable = true;
-          shellcheck.enable = true;
-          shfmt.enable = true;
-        };
-
-        settings.formatter.qmlformat = {
-          command = lib.getExe' pkgs.kdePackages.qtdeclarative "qmlformat";
-          options = [ "-i" ];
-          includes = [ "*.qml" ];
-        };
-      };
+    programs = {
+      just.enable = true;
+      nixfmt.enable = true;
+      prettier.enable = true;
+      qmlformat.enable = true;
+      shellcheck.enable = true;
+      shfmt.enable = true;
     };
+  };
 }
