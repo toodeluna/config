dbe03260516c4b37a56fd9e8b7bb50e9ea5dc7c7
Luna Heyman <luna@toodeluna.net>
2026-04-29 18:04:10 +0200

    refactor(packages/pipewire-rnnoise): move rnnoise configuration to separate package

--- nix/modules/hardware/audio.nix
+++ nix/modules/hardware/audio.nix
@@ -32,38 +32,9 @@ mkNixosModule {
         "context.properties"."default.clock.min-quantum" = 1024;
       };
 
-      extraConfig.pipewire."99-rnnoise"."context.modules" = lib.singleton {
-        name = "libpipewire-module-filter-chain";
-
-        args = {
-          "node.description" = "Noise cancelling source";
-          "media.name" = "Noise cancelling source";
-
-          "filter.graph".nodes = lib.singleton {
-            type = "ladspa";
-            name = "rnnoise";
-            label = "noise_suppressor_mono";
-            plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-
-            control = {
-              "VAD Threshold (%)" = 95;
-              "VAD Grace Period (ms)" = 200;
-              "Retroactive VAD Grace (ms)" = 0;
-            };
-          };
-
-          "capture.props" = {
-            "node.name" = "capture.rnnoise_source";
-            "audio.rate" = 48000;
-          };
-
-          "playback.props" = {
-            "node.name" = "rnnoise_source";
-            "media.class" = "Audio/Source";
-            "audio.rate" = 48000;
-          };
-        };
-      };
+      configPackages = [
+        pkgs.custom.pipewire-rnnoise
+      ];
     };
   };
 }
---
+++ nix/packages/pipewire-rnnoise/package.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  writeTextDir,
+
+  rnnoise-plugin,
+
+  priority ? 99,
+  vadThreshold ? 95,
+  vadGracePeriod ? 200,
+  vadGracePeriodRetroactive ? 0,
+}:
+let
+  path = "share/pipewire/pipewire.conf.d/${toString priority}-rnnoise.conf";
+  package = writeTextDir path (builtins.toJSON config);
+
+  config = {
+    name = "libpipewire-module-filter-chain";
+
+    args = {
+      "node.description" = "Noise cancelling source";
+      "media.name" = "Noise cancelling source";
+
+      "filter.graph".nodes = lib.singleton {
+        type = "ladspa";
+        name = "rnnoise";
+        label = "noise_suppressor_mono";
+        plugin = "${rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+
+        control = {
+          "VAD Threshold (%)" = vadThreshold;
+          "VAD Grace Period (ms)" = vadGracePeriod;
+          "Retroactive VAD Grace (ms)" = vadGracePeriodRetroactive;
+        };
+      };
+
+      "capture.props" = {
+        "node.name" = "capture.rnnoise_source";
+        "audio.rate" = 48000;
+      };
+
+      "playback.props" = {
+        "node.name" = "rnnoise_source";
+        "media.class" = "Audio/Source";
+        "audio.rate" = 48000;
+      };
+    };
+  };
+
+  meta = {
+    description = "A noise cancelling plugin configuration for pipewire";
+    platforms = lib.platforms.linux;
+    maintainers = [ lib.maintainers.toodeluna ];
+  };
+in
+package // { inherit meta; }
